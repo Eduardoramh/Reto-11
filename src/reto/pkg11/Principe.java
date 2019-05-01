c58d66333fb50d872ac65a93075948a1292f7de9
Eduardoramh <lalo.ramirezh102@gmail.com>
2019-05-01 18:22:58 -0500

    Reto 11

---
+++ src/reto/pkg11/Principe.java
@@ -0,0 +1,45 @@
+package reto.pkg11;
+
+/**
+ *
+ * @author lalor
+ */
+public class Principe {
+    private String nombre;
+    private int edad;
+    private double altura;
+
+    public Principe(String nombre, int edad, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+    
+    
+}
---
+++ src/reto/pkg11/Reto11.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto.pkg11;
+
+/**
+ *
+ * @author lalor
+ */
+public class Reto11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Principe personajeBuscado = new Principe("Augusto", 23, 1.85);
+        Principe propuesta2 = new Principe("Augusto", 23, 1.85);
+        Principe propuesta3 = new Principe("Guillermo", 30, 1.75);
+
+        if (personajeBuscado.getNombre().equals(propuesta3.getNombre()) 
+                && personajeBuscado.getEdad() == propuesta3.getEdad() 
+                && personajeBuscado.getAltura() == propuesta3.getAltura()){
+            System.out.println("Era al personaje que estaba buscando");
+        } else {
+            System.out.println("Seguiré buscando");
+        }
+
+        if (personajeBuscado.getNombre().equals(propuesta2.getNombre())) {
+            System.out.println("Era al personaje que estaba buscando");
+        } else {
+            System.out.println("Seguiré buscando");
+        }
+    }
+
+}
